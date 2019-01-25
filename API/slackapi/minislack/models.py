4f5ce7e95df54f939507467c20bc59bb085b9281
Guest <you@example.com>
2019-01-25 20:39:49 +0700

    dsa

--- API/slackapi/minislack/models.py
+++ API/slackapi/minislack/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+
+# class Channel(models.Model):
+#     channel_name = CharField(max_length=25)
+#
+#     def __str__(self):
+#         return self.channel_name
+
+
+class Conversation(models.Model):
+    # channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    user = models.CharField(max_length=100)
+    text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return '{}: {}'.format(self.user, self.text)
--- API/slackapi/minislack/views.py
+++ API/slackapi/minislack/views.py
@@ -1,23 +1,42 @@
 import requests
 from django.shortcuts import render
+from .models import Conversation
 
 
 def index(request):
-    channel_url = 'https://slack.com/api/users.conversations?token=xoxp-452524853910-452531064822-533500849926-4eb3cea535afed08434c384ef5236dae&types=public_channel%2C%20private_channel'
-    channel_response = requests.get(channel_url).json()
-    all_conversations = {}
-    for channel in channel_response['channels']:
-        id = channel['id']
-        conversation_url = 'https://slack.com/api/conversations.history?token=xoxp-452524853910-452531064822-533500849926-4eb3cea535afed08434c384ef5236dae&channel={}'.format(id)
-        conversation_response = requests.get(conversation_url).json()
-        messages = []
-        for conversation in conversation_response['messages']:
-            message = conversation['text']
-            if message.startswith('<!channel>'):
-                messages.append(message.replace('<!channel>', ''))
-        if messages:
-            all_conversations[channel['name_normalized']] = messages
+    legacy_token = 'xoxp-452524853910-452531064822-533011344403-ed164819e81bc463eabf73cc931aab29'
+    id = get_general_id(legacy_token)
+    get_messages(legacy_token, id)
+    messages = Conversation.objects.all()
     return render(request,
                   'minislack/index.html',
-                  {'all_conversations': all_conversations},
+                  {'messages': messages},
                   )
+
+
+def get_general_id(token):
+    channel_url = 'https://slack.com/api/users.conversations?token={}&types=public_channel%2C%20private_channel'.format(token)
+    channel_response = requests.get(channel_url).json()
+    for channel in channel_response['channels']:
+        if channel['is_general']:
+            return channel['id']
+
+
+def get_messages(token, channel_id):
+    conversation_url = 'https://slack.com/api/conversations.history?token={}&channel={}'.format(token, channel_id)
+    conversation_response = requests.get(conversation_url).json()
+    for conversation in conversation_response['messages']:
+        message = conversation['text']
+        if message.startswith('<!channel>'):
+            message = message.replace('<!channel>', '')
+            if message:
+                user_name = get_user_name(token, conversation['user'])
+                convo = Conversation(user=user_name,
+                                     text=message)
+                convo.save()
+
+
+def get_user_name(token, user_id):
+    user_url = 'https://slack.com/api/users.info?token={}&user={}'.format(token, user_id)
+    user_response = requests.get(user_url).json()
+    return user_response['user']['name']
